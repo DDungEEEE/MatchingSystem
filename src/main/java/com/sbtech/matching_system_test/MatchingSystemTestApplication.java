622e4bd6f0872188b8fdfad7dae03bceb4c183d8
DDungEEEE <thsdnjsdlr12@gmail.com>
2025-09-08 12:48:20 +0900

    project init

---
+++ src/main/java/com/sbtech/matching_system_test/MatchingSystemTestApplication.java
@@ -0,0 +1,13 @@
+package com.sbtech.matching_system_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingSystemTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchingSystemTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbtech/matching_system_test/MatchingSystemTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbtech.matching_system_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingSystemTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
